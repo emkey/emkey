f35411937b83fdddfeaa7a0d9ae02d36c748a80f
emkey <mllacsamana@olx.ph>
2017-03-05 20:21:26 +0800

    shortened text and change typeface

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
+
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
 ];
-
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
@@ -15,7 +15,7 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('default', ['sass'], function() {
